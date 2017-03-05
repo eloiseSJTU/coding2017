cf67e56fd433a9c7d152065ee87cf151c7aa2f50
Harry <chenhaowei93@163.com>
2017-03-05 17:21:42 +0800

    submit my second training
    完成ArrayUtil,并通过了测试；只对struts.xml做了解析，未能实现大部分功能

---
+++ group02/727171008/src/FirstCodeTest/ArrayListTest.java
@@ -0,0 +1,15 @@
+package FirstCodeTest;
+
+import org.junit.Before;
+import com.github.HarryHook.coding2017.basic.MyArrayList;
+
+
+public class ArrayListTest extends ListTest {
+	
+	@Before
+	public void setUpArrayList() 
+	{
+		aList = new MyArrayList();
+	}
+	
+}
